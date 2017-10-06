b453b6f32ae2f3cdef7ec4dc2409475a7f01d4f2
amitrajitbose <amitrajitbose9@gmail.com>
2017-10-06 21:27:36 +0530

    Add linear search program

---
+++ linear_search.py
@@ -0,0 +1,16 @@
+#Python programme to implement linear search
+import time
+def linearSearch(lst,key):
+	for i in range(len(lst)):
+		if(lst[i]==key):
+			print("found! It is at ", i)
+			return
+	print("the element is not in the array")
+
+mylist=[int(x) for x in (input("ENTER NUMBERS: ").strip().split(' '))]
+k=int(input("ENTER SEARCH ITEM: "))
+linearSearch(mylist,k)
+time.sleep(5)
+
+'''Uncomment Lines:2 and 13, if you want the console screen to wait for 5 seconds after 
+   the result has been shown'''
