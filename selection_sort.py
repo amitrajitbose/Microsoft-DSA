7d87e36b02f3007da7dc71c45f08e951106779ba
amitrajitbose <amitrajitbose9@gmail.com>
2017-10-06 21:05:50 +0530

    Add selection sort program

---
+++ selection_sort.py
@@ -0,0 +1,33 @@
+#Python Program For Implementing Selection Sort on n numbers
+#This Program also returns the number of swapping steps
+#import time
+def selectionSort(lst):
+	n=len(lst) #store the length of the list
+	c=0
+	for i in range(0,n-1):
+		index=0
+		s=lst[i]
+		'''In this list below, we are finding the smallest value in the subset of the list
+			that is from i+1 th index to n-1 th index and assign it to s and its index to
+			variable index'''
+		for j in range(i+1,n):
+			if(lst[j]<s):
+				index=j
+				s=lst[j]
+		if(index!=0):
+			c+=1
+			'''if any shifting is required, the index position would change from zero
+			   and hence we will swap them'''
+			temp=lst[i]
+			lst[i]=lst[index]
+			lst[index]=temp
+	print (lst)
+	return c
+
+mylist=[int(x) for x in (input("ENTER NUMBERS: ").strip().split(' '))]
+c=selectionSort(mylist)
+print (c , " swaps")
+#time.sleep(5)
+
+'''Uncomment Lines:3 and 30, if you want the console screen to wait for 5 seconds after 
+   the result has been shown'''
